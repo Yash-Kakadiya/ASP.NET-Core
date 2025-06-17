42d845baaaa0bbc22e13df74a523ce60d19a92e6
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2025-06-17 09:30:41 +0530

    Add files via upload

---
+++ Lab-03/Classes/Sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Classes
+{
+    abstract class Sum
+    {
+        public abstract int SumOfTwo(int a, int b);
+        public abstract int SumOfThree(int a, int b, int c);
+
+    }
+}
