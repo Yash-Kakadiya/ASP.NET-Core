3b3987fb47f83dde73099899994bd52f03d95f6c
Yash-Kakadiya <kakadiyayash77@gmail.com>
2025-06-21 10:40:40 +0530

    InitialLabs

---
+++ Lab02/Classes/Candidate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02.Classes
+{
+    class Candidate
+    {
+        int ID;
+        string Name;
+        int Age;
+        int Weight;
+        int Height;
+
+
+        public void GetCandidateDetails()
+        {
+            Console.Write("Enter ID: ");
+            ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.Write("Enter Name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("-------------------------------------------------");
+
+
+            Console.Write("Enter Age: ");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.Write("Enter Weight: ");
+            Weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------------");
+
+            Console.Write("Enter Height: ");
+            Height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public void DisplayCandidateDetails()
+        {
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("ID: " + ID);
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Weight: " + Weight);
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Height: " + Height);
+            Console.WriteLine("-------------------------------------------------");
+
+        }
+
+    }
+
+}
---
+++ Lab04/Classes/Wockhardt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04.Classes
+{
+    class Wockhardt : Hospital
+    {
+        public override void HospitalDetails()
+        {
+            Console.WriteLine("Wockhardt Hospital.");
+        }
+    }
+}
