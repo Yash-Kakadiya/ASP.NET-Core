4d17ba89801125689ccb7c65e4955a04d5ec0abf
Yash-Kakadiya <kakadiyayash77@gmail.com>
2025-07-18 09:26:01 +0530

    Change

--- Project(Hospital Management System)/HMS/Controllers/UserController.cs
+++ Project(Hospital Management System)/HMS/Controllers/UserController.cs
@@ -99,7 +99,6 @@ namespace HMS.Controllers
         {
             if (!ModelState.IsValid)
             {
-                Console.WriteLine( "Ok");
                 return View(userModel);
             }
 
--- Project(Hospital Management System)/HMS/obj/Debug/net8.0/HMS.AssemblyInfo.cs
+++ Project(Hospital Management System)/HMS/obj/Debug/net8.0/HMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e235b97a21a9d4b2f40fbe52299846088a47efe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97cf23dd51d0b466dbcfbe2783eb6df5d14188ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
